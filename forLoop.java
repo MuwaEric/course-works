e046172389b96c0a22e48deb5dcf789b192e052f
MuwaEric <168568414+MuwaEric@users.noreply.github.com>
2024-10-10 11:27:54 +0300

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,10 @@
+public class forLoop {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 20; a++) {
+            if (a % 2 == 0) {
+                System.out.println(a);
+            }
+        }
+
+    }
+}
