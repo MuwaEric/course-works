f296ac89d23d109c4958f3409eace23643d0a507
MuwaEric <168568414+MuwaEric@users.noreply.github.com>
2024-10-10 11:28:19 +0300

    Add files via upload

---
+++ Inner.java
@@ -0,0 +1,10 @@
+public class Inner {
+    public static void main(String[] args) {
+        Oop obj = new Oop();
+        obj.setAge(12);
+        obj.setName("eric");
+        System.out.println(obj.getAge() + obj.getName());
+
+    }
+    
+}
---
+++ InnerOOP.java
@@ -0,0 +1,31 @@
+class Oop {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+    public void setName(String num) {
+        name = num;
+    }
+}
+
+public class InnerOoP {
+    public static void main(String[] args) {
+        Oop obj = new Oop();
+        obj.setAge(12);
+        obj.setName("eric");
+        System.out.println(OOP.setAge + OOP.setName);
+
+    }
+    
+}
